903888cb4dec84f370abf6e41fb2ef1c430fa632
ap3334 <chwil2989@naver.com>
2022-09-29 11:43:34 +0900

    GenFile Entity 생성

---
+++ src/main/java/com/example/file/base/BaseConfig.java
@@ -0,0 +1,17 @@
+package com.example.file.base;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class BaseConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}

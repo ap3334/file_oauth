17ab0003c94f1d396b87caebf129382250d44243
ap3334 <chwil2989@naver.com>
2022-09-29 14:32:14 +0900

    csrf 토큰 비활성화

--- src/main/java/com/example/file/base/SecurityConfig.java
+++ src/main/java/com/example/file/base/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.file.base;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +13,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+
         return http.build();
     }
 
